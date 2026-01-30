100a656e41e4f7c2da4d81d4b2543e5be5b48de3
Nathan Dani_ <nathan.advir@gmail.com>
2026-01-30 12:46:10 +0000

    testes

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main(){
+    int idade;
+    int quantidade = 1;
+    float altura = 1.80;
+    char letra = 'a';
+    char nome[20] = "Nathan";
+    
+    printf("Qual o seu nome? ");
+    scanf("%s", nome);
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    
+    printf("olá mundo, você sabe que o %s estuda programação e ele tem %d anos?\n", nome, idade);
+
+    printf(" A altura do %s é: %.2f\n", nome, altura);
+   
+    }
