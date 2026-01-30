a9e0af0c26e6d69bcc98ce36c3a544eac31d2ee5
Nathan Dani_ <nathan.advir@gmail.com>
2026-01-30 10:01:38 -0300

    Add escola.c for student information input

---
+++ escola.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (){
+    int idade, matricula;
+    float altura;
+    char nome [50];
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    
+    printf("Digite sua altura: \n");
+    scanf("%f", &altura);
+    
+printf("Digite o seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("Digite sua matrícula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matrícula: %d \n", nome, matricula);
+    printf("Idade %d - altura: %f", idade, altura);
+    return 0;
+}
